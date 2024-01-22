0bf97f3a7221b1003171a95c4dc081e5c697f36f
Vlad93 <selukvlad@mail.ru>
2024-01-22 21:33:15 +0300

    feat: read file task done

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const stream = fs.createReadStream(
-  path.resolve(__dirname, './01-read-file', '/text.txt'),
+  path.resolve(__dirname, 'text.txt'),
   'utf-8',
 );
 let data = '';
